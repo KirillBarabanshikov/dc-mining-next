24eba53b2281c8b01429f89b8e41e5a37b0fa781
Kirill <kirillbar24@gmail.com>
2024-09-24 14:53:49 +0400

    added app icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,30 @@ import { QueryProvider } from './queryProvider';
 export const metadata: Metadata = {
     title: 'dc mining',
     description: 'dc mining',
+    icons: {
+        apple: {
+            url: '/apple-touch-icon.png',
+            sizes: '180x180',
+        },
+        icon: [
+            {
+                url: '/favicon-32x32.png',
+                sizes: '32x32',
+                type: 'image/png',
+            },
+            {
+                url: '/favicon-16x16.png',
+                sizes: '16x16',
+                type: 'image/png',
+            },
+            {
+                rel: 'mask-icon',
+                url: '/safari-pinned-tab.svg',
+                color: '#5bbad5',
+            },
+        ],
+    },
+    manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
