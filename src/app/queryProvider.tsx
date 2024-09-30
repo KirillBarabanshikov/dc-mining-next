f1eb19abd2fdb0ef8ed45ad09f150de82bc30ba1
Kirill <kirillbar24@gmail.com>
2024-09-30 11:36:40 +0400

    delete devtools

--- src/app/queryProvider.tsx
+++ src/app/queryProvider.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { isServer, QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { FC, PropsWithChildren } from 'react';
 
 function makeQueryClient() {
@@ -28,10 +27,5 @@ export function getQueryClient() {
 export const QueryProvider: FC<PropsWithChildren> = ({ children }) => {
     const queryClient = getQueryClient();
 
-    return (
-        <QueryClientProvider client={queryClient}>
-            {children}
-            <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-    );
+    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
