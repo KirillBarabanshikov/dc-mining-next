a84994805d8cb112fab44ffeb065e9a98e1ab9b9
Kirill <kirillbar24@gmail.com>
2024-10-18 13:45:48 +0400

    fix

--- src/entities/product/ui/RecentProductCard/RecentProductCard.tsx
+++ src/entities/product/ui/RecentProductCard/RecentProductCard.tsx
@@ -17,7 +17,7 @@ export const RecentProductCard: FC<IRecentProductCardProps> = ({ product }) => {
     const router = useRouter();
 
     return (
-        <article className={styles.card} onClick={() => router.push(`/product/${product.id}/${product.slug}`)}>
+        <article className={styles.card} onClick={() => router.push(`/product/${product.slug}`)}>
             <Image src={product.images[0]?.image || ''} alt={product.title} width={100} height={100} />
             <div>
                 <p className={styles.name}>{product.title}</p>
