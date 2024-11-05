fc946a0e4af397876313e6752bd9fa12ef261df2
Artem Okhotnikov <art.okhotnikov@yandex.ru>
2024-11-05 13:30:10 +0400

    feature: ui rework

--- src/shared/ui/IconButton/IconButton.tsx
+++ src/shared/ui/IconButton/IconButton.tsx
@@ -4,14 +4,28 @@ import { ButtonHTMLAttributes, FC, ReactNode } from 'react';
 import styles from './IconButton.module.scss';
 
 interface IIconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    icon: ReactNode;
-    className?: string;
+  icon: ReactNode;
+  className?: string;
+  variant?: 'outline';
+  rounded?: boolean;
 }
 
-export const IconButton: FC<IIconButtonProps> = ({ icon, className, onClick, ...props }) => {
-    return (
-        <button type={'button'} onClick={onClick} className={clsx(styles.iconButton, className)} {...props}>
-            {icon}
-        </button>
-    );
+export const IconButton: FC<IIconButtonProps> = ({
+  icon,
+  className,
+  variant = '',
+  rounded = '',
+  onClick,
+  ...props
+}) => {
+  return (
+    <button
+      type={'button'}
+      onClick={onClick}
+      className={clsx(styles.iconButton, className, styles[variant], rounded && styles.rounded)}
+      {...props}
+    >
+      {icon}
+    </button>
+  );
 };
