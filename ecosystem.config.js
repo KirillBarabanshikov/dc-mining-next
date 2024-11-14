1f7067eda2b72775aaa10678ba445267d9164544
Kirill <kirillbar24@gmail.com>
2024-11-14 13:38:36 +0400

    fix ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,10 @@ module.exports = {
         {
             name: 'dc-mining-next',
             exec_mode: 'cluster',
-            instances: 'max',
+            instances: 2,
             script: 'node_modules/next/dist/bin/next',
             args: 'start',
+            max_memory_restart: '500M',
         },
     ],
 };
