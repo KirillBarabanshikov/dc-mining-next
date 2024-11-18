db2e8ebf402618ee1850783500e5aea160feccbf
Kirill <kirillbar24@gmail.com>
2024-11-18 12:09:00 +0400

    исправлена микроразметка для страницы товара

--- src/widgets/ProductDetails/ProductDetails.tsx
+++ src/widgets/ProductDetails/ProductDetails.tsx
@@ -13,15 +13,11 @@ export const ProductDetails: FC<IProductDetailsProps> = ({ product }) => {
     if (!product) return <></>;
 
     return (
-        <div className={styles.details} itemScope itemType='https://schema.org/Product'>
-            <meta itemProp='name' content={product.seoTitle} />
-            <meta itemProp='description' content={product.seoDescription || ''} />
-            <meta itemProp='image' content={product.images[0]?.image} />
-            <meta itemProp='url' content={`https://dc-mining.ru/product/${product?.id}/${product?.slug}`} />
-
-            <div className={styles.info}>
+        <div className={styles.details}>
+            <div itemScope itemType='https://schema.org/Product' className={styles.info}>
                 <ProductSlider images={product.images} />
                 <ProductInfo product={product} />
+                <meta itemProp='brand' content={product.productSubCategory.title} />
             </div>
             <ProductsTabs product={product} />
         </div>
