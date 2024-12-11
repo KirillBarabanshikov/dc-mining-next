35da0798e7262f17deb8042cf131c351963f7e74
Kirill <kirillbar24@gmail.com>
2024-12-11 12:59:34 +0400

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,55 +13,55 @@ import { QueryProvider } from './queryProvider';
 export const revalidate = 0;
 
 export const metadata: Metadata = {
-    title: 'dc mining',
-    description: 'dc mining',
-    icons: {
-        apple: {
-            url: '/apple-touch-icon.png',
-            sizes: '180x180',
-        },
-        icon: [
-            {
-                url: '/favicon-32x32.png',
-                sizes: '32x32',
-                type: 'image/png',
-            },
-            {
-                url: '/favicon-16x16.png',
-                sizes: '16x16',
-                type: 'image/png',
-            },
-            {
-                rel: 'mask-icon',
-                url: '/safari-pinned-tab.svg',
-                color: '#5bbad5',
-            },
-        ],
+  title: 'dc mining',
+  description: 'dc mining',
+  icons: {
+    apple: {
+      url: '/apple-touch-icon.png',
+      sizes: '180x180',
     },
-    manifest: '/site.webmanifest',
-    other: {
-        'yandex-verification': 'a930c94ae40ba145',
-        'google-site-verification': '4Lq2UOlHmWA-VvtRjoszSPO46tQZksDQpljLY2r-vEU',
-    },
+    icon: [
+      {
+        url: '/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+      {
+        url: '/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'image/png',
+      },
+      {
+        rel: 'mask-icon',
+        url: '/safari-pinned-tab.svg',
+        color: '#5bbad5',
+      },
+    ],
+  },
+  manifest: '/site.webmanifest',
+  other: {
+    'yandex-verification': 'a930c94ae40ba145',
+    'google-site-verification': '4Lq2UOlHmWA-VvtRjoszSPO46tQZksDQpljLY2r-vEU',
+  },
 };
 
 export default async function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    // const response = await fetch(BASE_URL + '/api/settings');
-    // const data = await response.json();
+  // const response = await fetch(BASE_URL + '/api/settings');
+  // const data = await response.json();
 
-    return (
-        <html lang='ru'>
-            {/*<head dangerouslySetInnerHTML={{ __html: data.yandex }} />*/}
-            <body>
-                <Script
-                    id='yandex-metrika'
-                    strategy='afterInteractive'
-                    dangerouslySetInnerHTML={{
-                        __html: `(function (m, e, t, r, i, k, a) {
+  return (
+    <html lang='ru'>
+      {/*<head dangerouslySetInnerHTML={{ __html: data.yandex }} />*/}
+      <body>
+        <Script
+          id='yandex-metrika'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `(function (m, e, t, r, i, k, a) {
                   m[i] = m[i] || function () {
                     (m[i].a = m[i].a || []).push(arguments);
                   };
@@ -85,63 +85,65 @@ export default async function RootLayout({
                   accurateTrackBounce: true,
                   webvisor: true,
                 });`,
-                    }}
-                />
+          }}
+        />
 
-                <Script
-                    id='calltouch'
-                    strategy='afterInteractive'
-                    dangerouslySetInnerHTML={{
-                        __html: `(function(w,d,n,c){w.CalltouchDataObject=n;w[n]=function(){w[n]["callbacks"].push(arguments)};if(!w[n]["callbacks"]){w[n]["callbacks"]=[]}w[n]["loaded"]=false;if(typeof c!=="object"){c=[c]}w[n]["counters"]=c;for(var i=0;i<c.length;i+=1){p(c[i])}function p(cId){var a=d.getElementsByTagName("script")[0],s=d.createElement("script"),i=function(){a.parentNode.insertBefore(s,a)},m=typeof Array.prototype.find === 'function',n=m?"init-min.js":"init.js";s.async=true;s.src="https://mod.calltouch.ru/"+n+"?id="+cId;if(w.opera=="[object Opera]"){d.addEventListener("DOMContentLoaded",i,false)}else{i()}}})(window,document,"ct","a3jdr6dx");`,
-                    }}
-                />
+        <Script
+          id='calltouch'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,n,c){w.CalltouchDataObject=n;w[n]=function(){w[n]["callbacks"].push(arguments)};if(!w[n]["callbacks"]){w[n]["callbacks"]=[]}w[n]["loaded"]=false;if(typeof c!=="object"){c=[c]}w[n]["counters"]=c;for(var i=0;i<c.length;i+=1){p(c[i])}function p(cId){var a=d.getElementsByTagName("script")[0],s=d.createElement("script"),i=function(){a.parentNode.insertBefore(s,a)},m=typeof Array.prototype.find === 'function',n=m?"init-min.js":"init.js";s.async=true;s.src="https://mod.calltouch.ru/"+n+"?id="+cId;if(w.opera=="[object Opera]"){d.addEventListener("DOMContentLoaded",i,false)}else{i()}}})(window,document,"ct","a3jdr6dx");`,
+          }}
+        />
 
-                <Script
-                    src={'https://api-maps.yandex.ru/2.1/?apikey=3264ca32-f578-44c1-b743-f030dca1ba3a&lang=ru_RU'}
-                    strategy={'beforeInteractive'}
-                />
+        <Script
+          src={
+            'https://api-maps.yandex.ru/2.1/?apikey=3264ca32-f578-44c1-b743-f030dca1ba3a&lang=ru_RU'
+          }
+          strategy={'beforeInteractive'}
+        />
 
-                {/*<Script src={'https://challenges.cloudflare.com/turnstile/v0/api.js'} async defer />*/}
+        {/*<Script src={'https://challenges.cloudflare.com/turnstile/v0/api.js'} async defer />*/}
 
-                {/*  <Script*/}
-                {/*      id='mango-widget'*/}
-                {/*      strategy='afterInteractive'*/}
-                {/*      dangerouslySetInnerHTML={{*/}
-                {/*          __html: `(function (w, d, u, i, o, s, p) {*/}
-                {/*    if (d.getElementById(i)) { return; }*/}
-                {/*    w['MangoObject'] = o;*/}
-                {/*    w[o] = w[o] || function () {*/}
-                {/*      (w[o].q = w[o].q || []).push(arguments);*/}
-                {/*    };*/}
-                {/*    w[o].u = u;*/}
-                {/*    w[o].t = 1 * new Date();*/}
-                {/*    s = d.createElement('script');*/}
-                {/*    s.async = 1;*/}
-                {/*    s.id = i;*/}
-                {/*    s.src = u;*/}
-                {/*    p = d.getElementsByTagName('script')[0];*/}
-                {/*    p.parentNode.insertBefore(s, p);*/}
-                {/*})(window, document, '//widgets.mango-office.ru/widgets/mango.js', 'mango-js', 'mgo');*/}
+        {/*  <Script*/}
+        {/*      id='mango-widget'*/}
+        {/*      strategy='afterInteractive'*/}
+        {/*      dangerouslySetInnerHTML={{*/}
+        {/*          __html: `(function (w, d, u, i, o, s, p) {*/}
+        {/*    if (d.getElementById(i)) { return; }*/}
+        {/*    w['MangoObject'] = o;*/}
+        {/*    w[o] = w[o] || function () {*/}
+        {/*      (w[o].q = w[o].q || []).push(arguments);*/}
+        {/*    };*/}
+        {/*    w[o].u = u;*/}
+        {/*    w[o].t = 1 * new Date();*/}
+        {/*    s = d.createElement('script');*/}
+        {/*    s.async = 1;*/}
+        {/*    s.id = i;*/}
+        {/*    s.src = u;*/}
+        {/*    p = d.getElementsByTagName('script')[0];*/}
+        {/*    p.parentNode.insertBefore(s, p);*/}
+        {/*})(window, document, '//widgets.mango-office.ru/widgets/mango.js', 'mango-js', 'mgo');*/}
 
-                {/*mgo({ calltracking: { id: 33382 } });`,*/}
-                {/*      }}*/}
-                {/*  />*/}
-                <noscript>
-                    <div>
-                        <img
-                            src='https://mc.yandex.ru/watch/98130237'
-                            style={{ position: 'absolute', left: '-9999px' }}
-                            alt=''
-                            loading='lazy'
-                        />
-                    </div>
-                </noscript>
-                <QueryProvider>
-                    <LocationProvider>{children}</LocationProvider>
-                </QueryProvider>
-                <div id={'portal'} />
-                <div id={'overlay'}></div>
-            </body>
-        </html>
-    );
+        {/*mgo({ calltracking: { id: 33382 } });`,*/}
+        {/*      }}*/}
+        {/*  />*/}
+        <noscript>
+          <div>
+            <img
+              src='https://mc.yandex.ru/watch/98130237'
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=''
+              loading='lazy'
+            />
+          </div>
+        </noscript>
+        <QueryProvider>
+          <LocationProvider>{children}</LocationProvider>
+        </QueryProvider>
+        <div id={'portal'} />
+        <div id={'overlay'} />
+      </body>
+    </html>
+  );
 }
--- src/widgets/ProductDetails/ProductDetails.tsx
+++ src/widgets/ProductDetails/ProductDetails.tsx
@@ -6,20 +6,24 @@ import styles from './ProductDetails.module.scss';
 import { ProductInfo, ProductSlider, ProductsTabs } from './ui';
 
 interface IProductDetailsProps {
-    product?: IProduct | null;
+  product?: IProduct | null;
 }
 
 export const ProductDetails: FC<IProductDetailsProps> = ({ product }) => {
-    if (!product) return <></>;
+  if (!product) return <></>;
 
-    return (
-        <div className={styles.details}>
-            <div itemScope itemType='https://schema.org/Product' className={styles.info}>
-                <ProductSlider images={product.images} />
-                <ProductInfo product={product} />
-                <meta itemProp='brand' content={product.productSubCategory.title} />
-            </div>
-            <ProductsTabs product={product} />
-        </div>
-    );
+  return (
+    <div className={styles.details}>
+      <div
+        itemScope
+        itemType='https://schema.org/Product'
+        className={styles.info}
+      >
+        <ProductSlider images={product.images} />
+        <ProductInfo product={product} />
+        <meta itemProp='brand' content={product.productSubCategory?.title} />
+      </div>
+      <ProductsTabs product={product} />
+    </div>
+  );
 };
