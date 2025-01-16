1598aa24ec6bc54f818d9b39bbcabcd426fd8b14
Kirill <kirillbar24@gmail.com>
2025-01-16 15:29:19 +0400

    fix

--- src/entities/news/ui/NewsCard/NewsCard.tsx
+++ src/entities/news/ui/NewsCard/NewsCard.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 import Image from 'next/image';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { FC } from 'react';
 
 import { IMassMedia } from '@/entities/pageInfo/model';
@@ -17,32 +17,32 @@ interface INewsCardProps {
 
 export const NewsCard: FC<INewsCardProps> = ({ media, className }) => {
   const matches = useMediaQuery(MAX_WIDTH_MD);
+  const router = useRouter();
 
   const currentLink = media.link ? media.link : `/news/${media.slug}`;
 
   return (
-    <Link href={currentLink} target={media.link ? '_blank' : ''}>
-      <article className={clsx(styles.newsCard, className)}>
-        <Image
-          src={BASE_URL + media.image}
-          alt={media.title}
-          width={488}
-          height={240}
+    <article
+      className={clsx(styles.newsCard, className)}
+      onClick={() => router.push(currentLink)}
+    >
+      <Image
+        src={BASE_URL + media.image}
+        alt={media.title}
+        width={488}
+        height={240}
+      />
+      <div className={styles.cardBody}>
+        <time dateTime={media.dateAt}>{formatDate(media.dateAt)}</time>
+        <h5 className={styles.title}>{media.title}</h5>
+        <div
+          className={styles.subtitle}
+          dangerouslySetInnerHTML={{ __html: media.description }}
         />
-        <div className={styles.cardBody}>
-          <time dateTime={media.dateAt}>{formatDate(media.dateAt)}</time>
-          <h5 className={styles.title}>{media.title}</h5>
-          <div
-            className={styles.subtitle}
-            dangerouslySetInnerHTML={{ __html: media.description }}
-          />
-          <Link href={currentLink} target={media.link ? '_blank' : ''}>
-            <Button size={matches ? 'md' : 'lg'} className={styles.button}>
-              Подробнее
-            </Button>
-          </Link>
-        </div>
-      </article>
-    </Link>
+        <Button size={matches ? 'md' : 'lg'} className={styles.button}>
+          Подробнее
+        </Button>
+      </div>
+    </article>
   );
 };
