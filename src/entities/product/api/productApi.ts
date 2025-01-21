454b3e1b90afea91e5059ed63343bc9b699b9792
Kirill <kirillbar24@gmail.com>
2025-01-21 17:31:45 +0400

    fix compare

--- src/entities/product/api/productApi.ts
+++ src/entities/product/api/productApi.ts
@@ -3,61 +3,74 @@ import { instance } from '@/shared/api';
 import { mapProduct } from '../lib';
 import { IOrderProductBody, IProductDto } from './types';
 
-export const getProducts = async (params: { display?: boolean; title?: string } = {}) => {
-    try {
-        const response = await instance.get<IProductDto[]>('/products', { params });
-        return response.data.map(mapProduct);
-    } catch (e) {
-        console.error(e);
-        return null;
-    }
+export const getProducts = async (
+  params: { display?: boolean; title?: string } = {},
+) => {
+  try {
+    const response = await instance.get<IProductDto[]>('/products', { params });
+    return response.data.map(mapProduct);
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
 };
 
 export const getProductById = async (id: string | number) => {
-    try {
-        const response = await instance.get<IProductDto>(`/products/${id}`);
-        return mapProduct(response.data);
-    } catch (e) {
-        console.error(e);
-        return null;
-    }
+  try {
+    const response = await instance.get<IProductDto>(`/products/${id}`);
+    return mapProduct(response.data);
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
 };
 
 export const getProductBySlug = async (slug: string) => {
-    try {
-        const response = await instance.get<IProductDto>(`/product/slug?slug=${slug}`);
-        return mapProduct(response.data);
-    } catch (e) {
-        console.error(e);
-        return null;
-    }
+  try {
+    const response = await instance.get<IProductDto>(
+      `/product/slug?slug=${slug}`,
+    );
+    return mapProduct(response.data);
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
 };
 
 export const compareProducts = async (productId: number[]) => {
-    try {
-        const response = await instance.post<IProductDto[]>('/product/compare', { productId });
-        return response.data.map(mapProduct);
-    } catch (e) {
-        console.error(e);
-        return null;
-    }
+  try {
+    const response = await instance.post<
+      { category: string; count: number; products: IProductDto[] }[]
+    >('/product/compare', {
+      productId,
+    });
+    return response.data.map((data) => ({
+      ...data,
+      products: data.products.map(mapProduct),
+    }));
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
 };
 
 export const orderProduct = async (body: IOrderProductBody) => {
-    try {
-        await instance.post('/buy', body);
-    } catch (e) {
-        console.error(e);
-        throw new Error(`${e}`);
-    }
+  try {
+    await instance.post('/buy', body);
+  } catch (e) {
+    console.error(e);
+    throw new Error(`${e}`);
+  }
 };
 
 export const getProductsByIds = async (ids: number[]) => {
-    try {
-        const response = await instance.get<IProductDto[]>('/productsByIds', { params: { ids } });
-        return response.data.map(mapProduct);
-    } catch (e) {
-        console.error(e);
-        return null;
-    }
+  try {
+    const response = await instance.get<IProductDto[]>('/productsByIds', {
+      params: { ids },
+    });
+    return response.data.map(mapProduct);
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
 };
--- src/widgets/CompareList/CompareList.tsx
+++ src/widgets/CompareList/CompareList.tsx
@@ -1,43 +1,86 @@
 'use client';
 
 import { useMutation } from '@tanstack/react-query';
+import clsx from 'clsx';
 import { useEffect, useState } from 'react';
 import ScrollContainer from 'react-indiana-drag-scroll';
 
-import { compareProducts, IProduct, ProductCompareCard, useCompareStore } from '@/entities/product';
+import {
+  compareProducts,
+  IProduct,
+  ProductCompareCard,
+  useCompareStore,
+} from '@/entities/product';
 import { Switch } from '@/shared/ui';
 
 import styles from './CompareList.module.scss';
 
 export const CompareList = () => {
-    const [isOn, setIsOn] = useState(false);
-    const [compareList, setCompareList] = useState<IProduct[]>([]);
-    const { compare } = useCompareStore();
-    const { mutateAsync: compareMutate } = useMutation({ mutationFn: compareProducts });
+  const [isOn, setIsOn] = useState(false);
+  const [activeTab, setActiveTab] = useState(0);
+  const [compareList, setCompareList] = useState<
+    {
+      category: string;
+      count: number;
+      products: IProduct[];
+    }[]
+  >([]);
+  const { compare } = useCompareStore();
+  const { mutateAsync: compareMutate } = useMutation({
+    mutationFn: compareProducts,
+  });
 
-    useEffect(() => {
-        setProducts();
-    }, [compare]);
+  useEffect(() => {
+    setProducts();
+  }, [compare]);
 
-    const setProducts = async () => {
-        if (compare.length) {
-            await compareMutate(compare).then((data) => setCompareList(data ? data : []));
-        } else {
-            setCompareList([]);
-        }
-    };
+  const setProducts = async () => {
+    if (compare.length) {
+      await compareMutate(compare).then((data) =>
+        setCompareList(data ? data : []),
+      );
+    } else {
+      setCompareList([]);
+    }
+  };
 
-    return (
-        <>
-            <div className={styles.wrap}>
-                <span>Только отличия</span>
-                <Switch isOn={isOn} onClick={() => setIsOn(!isOn)} />
-            </div>
-            <ScrollContainer className={styles.list} nativeMobileScroll>
-                {compareList.map((product) => {
-                    return <ProductCompareCard key={product.id} product={product} onlyDifference={isOn} />;
-                })}
-            </ScrollContainer>
-        </>
-    );
+  return (
+    <>
+      <div className={styles.wrap}>
+        <span>Только отличия</span>
+        <Switch isOn={isOn} onClick={() => setIsOn(!isOn)} />
+      </div>
+      {!!compareList.length && (
+        <div className={clsx(styles.container, 'container')}>
+          <div className={styles.tabs}>
+            {compareList.map((item, index) => {
+              return (
+                <div
+                  key={item.category}
+                  className={clsx(
+                    styles.tab,
+                    activeTab === index && styles.active,
+                  )}
+                  onClick={() => setActiveTab(index)}
+                >
+                  {item.category}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
+      <ScrollContainer className={styles.list} nativeMobileScroll>
+        {compareList[activeTab]?.products.map((product) => {
+          return (
+            <ProductCompareCard
+              key={product.id}
+              product={product}
+              onlyDifference={isOn}
+            />
+          );
+        })}
+      </ScrollContainer>
+    </>
+  );
 };
