0da5a050a50d379e452592ce9fa3ab5c24048b35
Kirill <kirillbar24@gmail.com>
2025-02-10 17:14:37 +0400

    анимация в калькуляторе тоста

--- src/widgets/CalculatorToast/CalculatorToast.tsx
+++ src/widgets/CalculatorToast/CalculatorToast.tsx
@@ -7,6 +7,7 @@ import { FC } from 'react';
 import CloseIcon from '@/shared/assets/icons/close.svg';
 import DownloadIcon from '@/shared/assets/icons/download.svg';
 import calculatorMini from '@/shared/assets/images/calculator/calculator-mini.png';
+import { useIsSafari } from '@/shared/lib';
 
 import styles from './CalculatorToast.module.scss';
 
@@ -19,6 +20,7 @@ export const CalculatorToast: FC<ICalculatorToastProps> = ({
   isOpen,
   onClose,
 }) => {
+  const { isSafari } = useIsSafari();
   const router = useRouter();
 
   if (!isOpen) return <></>;
@@ -29,7 +31,13 @@ export const CalculatorToast: FC<ICalculatorToastProps> = ({
         onClick={() => router.push('/calculator')}
         className={styles.calculatorToast}
       >
-        <Image src={calculatorMini} alt={''} width={90} height={90} />
+        {isSafari ? (
+          <Image src={calculatorMini} alt={''} width={90} height={90} />
+        ) : (
+          <video autoPlay loop muted playsInline width={90} height={90}>
+            <source src={'/animations/calculator.webm'} />
+          </video>
+        )}
         <div className={styles.calculatorToastBody}>
           <div className={styles.calculatorToastTitle}>
             Калькулятор <span>доходности</span> оборудования
