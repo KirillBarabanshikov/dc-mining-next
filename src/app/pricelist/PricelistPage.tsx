696468f6ab3c2c46e10c0ec688fd2eb8eeab89d0
Kirill <kirillbar24@gmail.com>
2025-03-11 17:34:49 +0400

    feat: added pricelist page

---
+++ src/app/pricelist/PricelistPage.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
+
+import { instance } from '@/shared/api';
+import { BASE_URL } from '@/shared/consts';
+
+const PricelistPage = () => {
+  const { data } = useQuery({
+    queryKey: ['pdfs'],
+    queryFn: async () => {
+      const response = await instance.get<{ id: number; media: string }>(
+        '/pdfs',
+      );
+      return response.data;
+    },
+  });
+
+  const { mutateAsync } = useMutation({
+    mutationFn: async () => {
+      await instance.post('/statistics', {
+        count: 0,
+        dateTime: 'string',
+      });
+    },
+  });
+
+  useEffect(() => {
+    mutateAsync();
+  }, []);
+
+  return (
+    <>
+      {data && (
+        <iframe src={BASE_URL + data.media} width='100%' height='100%' />
+      )}
+    </>
+  );
+};
+
+export default PricelistPage;
---
+++ src/app/pricelist/page.tsx
@@ -0,0 +1,5 @@
+import PricelistPage from './PricelistPage';
+
+export default async function Page() {
+  return <PricelistPage />;
+}
