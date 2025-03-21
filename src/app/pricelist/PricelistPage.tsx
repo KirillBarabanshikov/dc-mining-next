44d7dceff0f77f05652fa10c4228227bd3dccbe9
Kirill <kirillbar24@gmail.com>
2025-03-21 17:05:04 +0400

    fix price list page

--- src/app/pricelist/PricelistPage.tsx
+++ src/app/pricelist/PricelistPage.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import { useMutation, useQuery } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 import { instance } from '@/shared/api';
 import { BASE_URL } from '@/shared/consts';
 
 const PricelistPage = () => {
+  const router = useRouter();
+
   const { data } = useQuery({
     queryKey: ['pdfs'],
     queryFn: async () => {
@@ -27,26 +30,18 @@ const PricelistPage = () => {
   });
 
   useEffect(() => {
-    mutateAsync();
-  }, []);
+    const sendStatistic = async () => {
+      await mutateAsync();
 
-  return (
-    <>
-      {data && (
-        // <object
-        //   data={BASE_URL + data.media}
-        //   type='application/pdf'
-        //   width='100%'
-        //   height='100%'
-        // >
-        //   <a href={BASE_URL + data.media} target='_blank'>
-        //     ПРАЙС-ЛИСТ
-        //   </a>
-        // </object>
-        <iframe src={BASE_URL + data.media} width={300} height={600} />
-      )}
-    </>
-  );
+      if (data?.media) {
+        router.replace(BASE_URL + data.media);
+      }
+    };
+
+    sendStatistic();
+  }, [data, router]);
+
+  return <p>Перенаправление</p>;
 };
 
 export default PricelistPage;
