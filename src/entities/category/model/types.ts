f798b2dc38c27cec5e2a5ec8918e581a7fee3698
Kirill <kirillbar24@gmail.com>
2025-03-25 14:29:32 +0400

    merge

--- src/entities/category/model/types.ts
+++ src/entities/category/model/types.ts
@@ -17,23 +17,5 @@ export interface ICategory {
   slug: string;
   link?: string;
   seoName: string;
-
-  // id: number;
-  // title: string;
-  // image: string;
-  // display: boolean;
-  // displayHeader: boolean;
-  // subCategory: {
-  //   id: number;
-  //   title: string;
-  //   slug: string;
-  //   image?: string;
-  // }[];
-  // images: {
-  //   id: number;
-  //   image?: string;
-  // }[];
-  // name: string;
-  // slug: string;
-  // link?: string;
+  displaySidebar: boolean;
 }
