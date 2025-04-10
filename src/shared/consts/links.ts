f696fe796c7c1e64c58cb1dbe3c1fa5ac326a774
Kirill <kirillbar24@gmail.com>
2025-04-10 14:14:12 +0400

    fix

--- src/shared/consts/links.ts
+++ src/shared/consts/links.ts
@@ -1,16 +1,17 @@
 export const SERVICES_LINKS = [
-    { title: 'Лизинг', path: '/leasing' },
-    { title: 'Готовый бизнес для майнинга криптовалют', path: '/readyBusiness' },
-    { title: 'Ремонт и сервис', path: '/service' },
-    { title: 'Размещение в дата центре', path: '/data-center' },
+  { title: 'Лизинг', path: '/leasing' },
+  { title: 'Готовый бизнес для майнинга криптовалют', path: '/readyBusiness' },
+  { title: 'Ремонт и сервис', path: '/service' },
+  { title: 'Размещение в дата центре', path: '/data-center' },
+  { title: 'Строительство ЦОД', path: '/data-center-development' },
 ];
 
 export const ABOUT_LINKS = [
-    { title: 'Доставка', path: '/delivery' },
-    { title: 'Оплата', path: '/payment' },
-    { title: 'О компании', path: '/about' },
-    { title: 'СМИ о нас', path: '/news' },
-    { title: 'FAQ', path: '/faq' },
-    { title: 'Полезные ссылки', path: '/links' },
-    { title: 'Контакты', path: '/contacts' },
+  { title: 'Доставка', path: '/delivery' },
+  { title: 'Оплата', path: '/payment' },
+  { title: 'О компании', path: '/about' },
+  { title: 'СМИ о нас', path: '/news' },
+  { title: 'FAQ', path: '/faq' },
+  { title: 'Полезные ссылки', path: '/links' },
+  { title: 'Контакты', path: '/contacts' },
 ];
